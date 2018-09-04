b3e4177ef9354408796d29a4197d5125310a305a
wemersonw8 <wemersonw8@wemerson>
2018-09-04 19:52:36 -0300

    java.lang.NoClassDefFoundError:  org/hibernate/internal/CoreMessageLogger

---
+++ meuprimeiroprojetojsf/src/main/java/br/com/cursojsf/Teste.java
@@ -0,0 +1,13 @@
+package br.com.cursojsf;
+
+import java.util.ArrayList;
+
+public class Teste {
+	public static void main (String [] args) {
+		ArrayList<String> alunos = new ArrayList<>();
+		
+		alunos.add("wemerson");
+		
+	System.out.println(alunos.get(0));
+	}
+}
